2eeab07a45abaf07d5e74e590cd50140cea1731c
jackbang <jackbang0114@gmail.com>
2021-04-11 23:59:17 +0800

    Added some comments

--- ecouProject/src/pages/StoreInfo/StoreInfo.jsx
+++ ecouProject/src/pages/StoreInfo/StoreInfo.jsx
@@ -64,7 +64,11 @@ class StoreInfo extends Component {
 
     //console.log(`The windows height is ${windowHeight_rpx}`);
     //console.log(`The screen  height is ${screenHeight_rpx}`);
-    
+
+    //下面这部分是用来控制scroll-view的，这部分的height尤其关键
+    //这部分需要先将页面disableScroll: true
+    //其次计算出当前window的rpx单位的高度，减去padding的rpx高度，再减去searchBar+StoreInfo部分的400rpx，再减去Tabs的110rpx
+    //用px单位的话，会出现设备不适配的情况
     const scrollTop = 0
     const Threshold = 100
     var scrollStyle = {
@@ -72,20 +76,23 @@ class StoreInfo extends Component {
     }
 
     return (
-      <View className='store-info-page'>
-        <View className='at-col' style={{padding: `${top_height}px 0px 0px 0px`}}>
-          <View className='at-col' style='height:400rpx'>
+      <View className='store-info-page' /* 这个标签主要是用来放background*/>
+        <View className='at-col' style={{padding: `${top_height}px 0px 0px 0px`}} /* 这个页面用来放 1.searchBar和下面的StoreInfo 2.AtTabs 为了避免和耳朵重叠，这里用了padding*/>
+
+          <View className='at-col' style='height:400rpx' /* 这里是*/>
+
             <AtSearchBar
               showActionButton
               value={this.state.value}
               onChange={this.onChange.bind(this)}
               onActionClick={this.onActionClick.bind(this)}
             />
-            <View className='at-row store-info-background-img'>
-              <View className='at-row store-pic-position-info' style={{width: `${system_width}px`}}> 
+
+            <View className='at-row store-info-background-img' /* 这里是StoreInfo背景那个不规则图形*/>
+              <View className='at-row store-pic-position-info' style={{width: `${system_width}px`}} /* 这里是用来规划image放置的位置 */> 
                 <AtAvatar className='store-pic-info' image={store_pic}></AtAvatar>
               </View>
-              <View className='at-col'> 
+              <View className='at-col' /*这里写的是StoreInfo 文字部分*/> 
                 <View className='at-col store-name-position-info'>
                   京剧馆·剧本杀·狼人杀
                 </View>
@@ -102,7 +109,9 @@ class StoreInfo extends Component {
                 </View>
               </View>
             </View>
+
           </View>
+
           <View className='at-row' style='background-color:#ffffff'>
             <AtTabs /* TODO: 这部分需要重构，红点没实现，列表不同日期显示不同灰度也没实现 */
               current={this.state.current}
@@ -130,17 +139,17 @@ class StoreInfo extends Component {
                 >
                   <View className='at-row queue-tab-info'>
                     {/*  每个tab上信息显示 */}
-                    <View className='at-row play-pic-position-info' style='width:21vw'>
+                    <View className='at-row play-pic-position-info' style='width:21vw' /* 这里写的是 每个tab上剧本图片的位置*/>
                       <image className='play-pic-info' src={play_pic}>
                       <text className='play-pic-label-info'>本格</text>
                       </image>
                     </View>
-                    <View className='at-col play-intro-info'>
+                    <View className='at-col play-intro-info' /*这里的信息是每个tab上 剧本的一些文字信息 */>
                       <View className='at-col play-name-position-info'>木兮僧之戏</View>
-                      <View className='at-row'>
-                        <View className='at-col'>
+                      <View className='at-row' /* =- 这一部分是这样，两列，第一列有两行文字，第二列用来放按钮 */>
+                        <View className='at-col' /* 第一列 有两行*/>
                           <View className='at-row play-time-position-info'><text decode="{{true}}">04月03日&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;13:00</text></View>
-                          <View className='at-row play-headcount-position-info'>
+                          <View className='at-row play-headcount-position-info' /* 这一部分有三列 */>
                             <View className='play-headcount-info'><text decode="{{true}}">人数：7/10</text></View>
                             <View className='play-male-position-info'>
                               <image className='gender-icon-info' src={male_icon}></image>
@@ -154,8 +163,8 @@ class StoreInfo extends Component {
 
                           </View>
                         </View>
-                        <View className='at-row' style='width:20vw'>
-                          {/* Button */}
+                        <View className='at-row' style='width:20vw' /*第二列是用来放按钮 */>
+                          {/* Button  激活与不激活 具体看taroui中的文档*/}
                           <AtButton type='primary' circle='true'>我要上车</AtButton>
                         </View>
                       </View>
@@ -209,7 +218,7 @@ class StoreInfo extends Component {
                   <View className='at-row queue-tab-info'></View>
                   <View className='at-row queue-tab-info'></View>
                   <View className='at-row queue-tab-info'></View>
-                  <View className='at-row tab-blank'></View>
+                  <View className='at-row tab-blank'></View> {/*切记，每个AtTabsPane最下面要加一小条空白，否则阴影部分显示不全，会很难看 */}
                 </ScrollView>
               </AtTabsPane>
               <AtTabsPane current={this.state.current} index={1}>
